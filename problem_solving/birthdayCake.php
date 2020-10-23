9518437d8ec7e5803037bb7a1bdf593b7638abb6
choling <cholingken@gmail.com>
2020-10-23 22:51:36 +0800

    Update new problem solution

---
+++ problem_solving/birthdayCake.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * Complete the 'birthdayCakeCandles' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY candles as parameter.
+ */
+
+function birthdayCakeCandles($candles) 
+{
+    // Write your code here
+    $candleType = array_count_values($candles);
+    krsort($candleType);
+    // foreach ($candleType as $key => $val) {
+    //     echo "$key = $val\n";
+    // }
+    return array_values($candleType)[0];
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$candles_count = intval(trim(fgets(STDIN)));
+
+$candles_temp = rtrim(fgets(STDIN));
+
+$candles = array_map('intval', preg_split('/ /', $candles_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+$result = birthdayCakeCandles($candles);
+
+fwrite($fptr, $result . "\n");
+
+fclose($fptr);

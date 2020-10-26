25ac3859788e463225f602dde0d34d91b7383c71
choling <cholingken@gmail.com>
2020-10-26 22:51:22 +0800

    Update day 9 solution

---
+++ 30_days_of_code/day9_recursion.php
@@ -0,0 +1,23 @@
+<?php
+
+// Complete the factorial function below.
+function factorial($n) {
+    If ($n <= 1) {
+        return $n;
+    } else {
+        return $n * factorial($n - 1);
+    }
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$stdin = fopen("php://stdin", "r");
+
+fscanf($stdin, "%d\n", $n);
+
+$result = factorial($n);
+
+fwrite($fptr, $result . "\n");
+
+fclose($stdin);
+fclose($fptr);
